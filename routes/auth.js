03d58fa3f0a21be211394eba7653f70311227473
Manjul <manjultamrakar4@gmail.com>
2022-07-26 16:23:21 +0545

    had forgotten to import the login validation🤦‍♂️

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const {registerValidation} = require('../validation');
+const {registerValidation, loginValidation} = require('../validation');
 const bcrypt = require('bcryptjs');
 
 //register
